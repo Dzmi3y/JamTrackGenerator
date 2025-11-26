1823f225f9cc86a07afd6c3dc5168de90ffe2240
Dzmitry <dzmi3y@gmail.com>
2025-11-26 23:50:21 +0300

    feat: add preloader and project structure after init

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((res) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, res.clone());
+            return res;
+          });
+        })
+      );
+    })
+  );
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById("root")!);
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+const preloader = document.getElementById("preloader");
+if (preloader) {
+  preloader.classList.add("fade-out");
+  setTimeout(() => preloader.remove(), 500);
+}
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((reg) => console.log("Service Worker registered:", reg))
+      .catch((err) =>
+        console.error("Service Worker registration failed:", err)
+      );
+  });
+}
