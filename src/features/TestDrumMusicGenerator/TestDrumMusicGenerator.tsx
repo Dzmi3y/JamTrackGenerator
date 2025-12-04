2947c27118f950fe305012b33007b856b91fde87
Dzmitry <dzmi3y@gmail.com>
2025-12-04 16:10:31 +0300

    feat: Display time

--- src/features/TestDrumMusicGenerator/TestDrumMusicGenerator.tsx
+++ src/features/TestDrumMusicGenerator/TestDrumMusicGenerator.tsx
@@ -12,32 +12,59 @@ const TestDrumMusicGenerator: React.FC = () => {
 
   const { getDefaultDrumPart } = useDrumPart();
   const { getInstrumentPart } = useInstrumentPart();
+  const partResultInst = getInstrumentPart("D3 G3 C3 F3", "1", bpm);
+  const partResult = getDefaultDrumPart(bpm);
+
+  const [transportTime, setTransportTime] = useState(0);
+  const [transportPosition, setTransportPosition] =
+    useState<Tone.Unit.Time>("0:0:0");
 
   const play = async () => {
+    Tone.Transport.timeSignature = [4, 4];
     Tone.Transport.cancel();
     Tone.Transport.stop();
 
     await Tone.start();
     Tone.Transport.bpm.value = bpm;
 
-    const partResultInst = getInstrumentPart("D3 G3 C3 F3", "1", bpm);
-    const partResult = getDefaultDrumPart(bpm);
-    if (!partResult) {
+    if (!partResult || !partResultInst) {
       return;
     }
-    console.log("resultArray");
-    console.log(partResult.part);
 
-    pianoPart.playPart(partResultInst, true);
-    drumsPart.playPart(partResult, true);
+    pianoPart.playPart(partResultInst, false);
+    drumsPart.playPart(partResult, false);
+
     Tone.Transport.start();
   };
+
+  const stop = () => {
+    Tone.Transport.stop();
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTransportTime(Tone.Transport.seconds);
+      setTransportPosition(Tone.Transport.position);
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div>
       <button onClick={play} disabled={pianoPart.isLoading}>
-        {pianoPart.isLoading ? "Loading Drumkit..." : "▶ Play with Drumkit"}
+        {pianoPart.isLoading
+          ? "Loading Instruments..."
+          : "▶ Play with Instruments"}
       </button>
+      <button onClick={stop} disabled={pianoPart.isLoading}>
+        {pianoPart.isLoading ? "Loading Instruments..." : "Stop"}
+      </button>
+
+      <div style={{ marginTop: "1rem" }}>
+        <p>Transport time: {transportTime.toFixed(2)}s</p>
+        <p>Position: {transportPosition.toString()}</p>
+      </div>
     </div>
   );
 };
