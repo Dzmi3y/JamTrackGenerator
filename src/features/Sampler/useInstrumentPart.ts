c0641f5f644b467697c88285e6e9861095d7729f
Dzmitry <dzmi3y@gmail.com>
2025-12-04 15:48:23 +0300

    feat: add useInstrumentPart

---
+++ src/features/Sampler/useInstrumentPart.ts
@@ -0,0 +1,35 @@
+import { useCallback } from "react";
+import { usePattern } from "./usePattern";
+import {
+  usePartBuilder,
+  type PartResult,
+} from "../../shared/hooks/usePartBuilder";
+
+export function useInstrumentPart() {
+  const { getPart } = usePartBuilder();
+  const { getPartInfo } = usePattern();
+
+  const getInstrumentPart = useCallback(
+    (
+      note: string | string[] | string[][],
+      id: string,
+      bpm: number
+    ): PartResult | undefined => {
+      const partInfo = getPartInfo(note, id);
+
+      if (!partInfo) {
+        return undefined;
+      }
+
+      const part = getPart(partInfo!, bpm);
+
+      return {
+        totalDuration: part.totalDuration,
+        part: part.part,
+      };
+    },
+    [getPart, getPartInfo]
+  );
+
+  return { getInstrumentPart };
+}
--- src/features/TestDrumMusicGenerator/TestDrumMusicGenerator.tsx
+++ src/features/TestDrumMusicGenerator/TestDrumMusicGenerator.tsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import * as Tone from "tone";
 import { useDrumPart } from "../Sampler/useDrumPart";
 import { useTonePart } from "../../shared/hooks/useTonePart";
+import { useInstrumentPart } from "../Sampler/useInstrumentPart";
 
 const TestDrumMusicGenerator: React.FC = () => {
   const bpm = 180;
 
-  const { playPart, isLoading } = useTonePart("drums");
+  const drumsPart = useTonePart("drums");
+  const pianoPart = useTonePart("piano");
 
   const { getDefaultDrumPart } = useDrumPart();
+  const { getInstrumentPart } = useInstrumentPart();
 
   const play = async () => {
     Tone.Transport.cancel();
@@ -17,20 +20,23 @@ const TestDrumMusicGenerator: React.FC = () => {
     await Tone.start();
     Tone.Transport.bpm.value = bpm;
 
+    const partResultInst = getInstrumentPart("D3 G3 C3 F3", "1", bpm);
     const partResult = getDefaultDrumPart(bpm);
     if (!partResult) {
       return;
     }
     console.log("resultArray");
     console.log(partResult.part);
-    playPart(partResult, true);
+
+    pianoPart.playPart(partResultInst, true);
+    drumsPart.playPart(partResult, true);
     Tone.Transport.start();
   };
 
   return (
     <div>
-      <button onClick={play} disabled={isLoading}>
-        {isLoading ? "Loading Drumkit..." : "▶ Play with Drumkit"}
+      <button onClick={play} disabled={pianoPart.isLoading}>
+        {pianoPart.isLoading ? "Loading Drumkit..." : "▶ Play with Drumkit"}
       </button>
     </div>
   );
